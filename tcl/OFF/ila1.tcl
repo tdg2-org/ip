a9587d67282b29af6aeebe51355235cfd056a4f3
Tony <tonydgoodwin@gmail.com>
2025-08-27 11:37:41 -0400

    automated commit & push

--- tcl/OFF/ila1.tcl
+++ tcl/OFF/ila1.tcl
@@ -1,15 +1,17 @@
-#set ipDir "../ip"
+set ipDir "../ip"
 set modName "ila1"
 
-#if {![file exists $ipDir]} {error "ip directory not present"}
+if {![file exists $ipDir]} {error "ip directory not present"}
 
-#create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name $modName -dir $ipDir -force
-create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name $modName -force -dir ./
+create_ip -name ila -vendor xilinx.com -library ip -version 6.2 -module_name $modName -dir $ipDir -force
 set_property -dict [list \
-  CONFIG.C_DATA_DEPTH {16384} \
-  CONFIG.C_NUM_OF_PROBES {2} \
-  CONFIG.C_PROBE0_WIDTH {6} \
-  CONFIG.C_PROBE1_WIDTH {6} \
+  CONFIG.C_DATA_DEPTH {1024} \
+  CONFIG.C_NUM_OF_PROBES {5} \
+  CONFIG.C_PROBE0_WIDTH {64} \
+  CONFIG.C_PROBE1_WIDTH {64} \
+  CONFIG.C_PROBE2_WIDTH {3} \
+  CONFIG.C_PROBE3_WIDTH {3} \
+  CONFIG.C_PROBE4_WIDTH {4} \
 ] [get_ips $modName]
 
 if {"-gen" in $argv} {generate_target all [get_files $modName.xci]}
@@ -22,3 +24,4 @@ if {"-gen" in $argv} {generate_target all [get_files $modName.xci]}
 #  CONFIG.C_TRIGOUT_EN {true} \
 #  CONFIG.ALL_PROBE_SAME_MU_CNT {4} \     ## max=16
 #  CONFIG.C_DATA_DEPTH {2048} \ 1024,2048,4096,8192,16384,32768,65536,131072
+
